7e66c02126c211caafdc47352a7207326c128874
Vishnuvm451 <vishnu789451@gmail.com>
2025-12-11 20:06:18 +0530

    change

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  // ...
+  //// Add the dependency for the Google services Gradle plugin
 
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.4" apply false
+  
+  id("com.google.gms.google-services") version "4.4.1" apply false
 
 }
 
