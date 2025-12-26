15c69314e1c652cbbe271d426d040c0f46ca99a5
Vishnuvm451 <vishnu789451@gmail.com>
2025-12-26 21:25:05 +0530

    new

---
+++ lib/internal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InternalMarksPage extends StatelessWidget {
+  const InternalMarksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
