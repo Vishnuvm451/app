c20cf875d3d468698437617adc103412cb6b258e
Vishnuvm451 <vishnu789451@gmail.com>
2026-02-11 04:51:46 +0530

    added notification trigger

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,16 @@ import 'package:darzo/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'auth/firebase_options.dart';
 import 'auth/auth_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   warmUpApiServer();
+
+  pingRenderAPI();
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => AppAuthProvider())],
