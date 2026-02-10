0236de3c5ed4419be05be386d3decaea1f0dbbef
Vishnuvm451 <vishnu789451@gmail.com>
2026-02-11 04:54:59 +0530

    location config clg lat and long

---
+++ lib/auth/location_config.dart
@@ -0,0 +1,9 @@
+//10.766114648621784, 76.40608013362095
+
+class LocationConfig {
+  // 📍  COLLEGE COORDINATES
+  static const double collegeLat = 10.766114648621784;
+  static const double collegeLng = 76.40608013362095;
+
+  static const double allowedRadiusMeters = 350;
+}
